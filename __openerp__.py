32a0d332136c55ad964b9967e8ded6b81706a9d2
developeruser <rroker@greenlightip.com>
2016-01-09 21:48:27 -0800

    Major update, tasks, emails, filters and searches

--- __openerp__.py
+++ __openerp__.py
@@ -23,7 +23,7 @@
     # for the full list
     'category': 'Projects, SRED, Applications, Canada',
 
-    'version': '0.5',
+    'version': '3.0',
     'sequence': 11,
 
     # any module necessary for this one to work correctly
