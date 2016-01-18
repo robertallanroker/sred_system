d46a87b3fd6afdaf4bb2fdc3b3434c55b4108b0f
developeruser <rroker@greenlightip.com>
2016-01-18 09:19:01 -0800

    Major update, tasks, emails, filters and searches

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ import model_sred_status
 import model_work_roles
 import model_work_folders
 import models
-import mods_to_existing_modules
 import model_sred_projects
 
 
--- mods_to_existing_modules.py
+++ mods_to_existing_modules.py
@@ -1,7 +1,3 @@
 from openerp import models, fields, api, osv
 
-class my_sred_project_class(models.Model):
-    _name = 'sred_system.sred_project'
-    a_claim_project = fields.Char()
-    # Test
 
